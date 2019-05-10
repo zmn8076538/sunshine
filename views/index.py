9b128b0d4d8f8947f77eeefd65aad7b1179356e8
zmn8076538 <zmn_job@sina.com>
2019-05-10 15:25:35 +0800

    insert index.html

--- views/index.py
+++ views/index.py
@@ -1,8 +1,12 @@
 from views import app
 from flask import render_template
 
-@app.route('/login')
-def user_login():
+@app.route('/index')
+def index():
 
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
     return render_template('login.html')
 
