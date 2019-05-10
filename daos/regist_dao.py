e256dd1f39c24e5d57aa668e442a50a89034332c
zmn8076538 <zmn_job@sina.com>
2019-05-10 20:00:28 +0800

    regist

--- daos/regist_dao.py
+++ daos/regist_dao.py
@@ -3,20 +3,13 @@ from cfg import cursor
 def is_exist_username(username):
     sql = "select * from user where user_name = '{user_name}'".format(user_name=username)
     cursor.execute(sql)
-    if cursor.rowcount == 1:
-        return False
-    else:
-        return True
+    return cursor.rowcount == 1
 
 def regist_in_db(username,password):
-    if is_exist_username(username):
         sql = "insert into user(user_name,user_password) values('{username}','{password}')"\
             .format(username=username,password=password)
         cursor.execute(sql)
-        if cursor.rowcount == 1:
-            return True
-    else:
-        raise Exception('用户名已存在')
+        return cursor.rowcount == 1
 
 
 
--- services/regist_service.py
+++ services/regist_service.py
@@ -1,11 +1,10 @@
-from daos.regist_dao import regist_in_db
+from daos.regist_dao import regist_in_db,is_exist_username
 
 def regist_by_upr(username,password,repassword):
     if password == repassword:
-        if regist_in_db(username,password):
-            print('注册成功')
-            return True
+        if is_exist_username(username):
+            raise Exception('用户名已存在')
         else:
-            raise Exception('注册失败')
+            return regist_in_db(username,password)
     else:
         raise Exception('两次输入密码不一致')
