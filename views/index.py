28dbecfa4c5c46f9d5b2ab0b15dd076f13009485
zmn8076538 <zmn_job@sina.com>
2019-05-10 15:05:04 +0800

    josdf

---
+++ views/index.py
@@ -0,0 +1,8 @@
+from views import app
+from flask import render_template
+
+@app.route('/login')
+def user_login():
+
+    return render_template('index.html')
+
--- views/regist.py
+++
@@ -1,8 +0,0 @@
-from views import app
-from flask import render_template
-
-@app.route('/regist')
-def user_regist():
-
-    return render_template('regist.html')
-
