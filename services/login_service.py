be0decc8fa3d89e806fc3bcf70f8bfcbdc17495e
zmn8076538 <zmn_job@sina.com>
2019-05-13 18:22:19 +0800

    login ajax

--- services/login_service.py
+++ services/login_service.py
@@ -2,5 +2,4 @@ from daos.login_dao import login_get_password_by_username
 
 def login_by_username_password(username,password):
     password_in_db = login_get_password_by_username(username)
-    if password == password_in_db:
-        return True
+    return password == password_in_db
--- views/index.py
+++ views/index.py
@@ -1,26 +1,25 @@
 from views import app
-from flask import render_template,jsonify
+from flask import render_template,jsonify,redirect,url_for
 from flask import request
 from services.login_service import login_by_username_password
 from services.regist_service import regist_by_upr
 
 @app.route('/index')
 def index():
-
     return render_template('index.html')
+
+@app.route('/regist_index',methods=['GET'])
+def regist_index():
+    return render_template('regist.html')
 
 @app.route('/login',methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
     if login_by_username_password(username,password):
-        return render_template('home.html')
+        return jsonify({'message':'success'})
     else:
-        return render_template('index.html')
-
-@app.route('/regist_index',methods=['GET'])
-def regist_index():
-    return render_template('regist.html')
+        return jsonify({'message':'用户或密码错误'})
 
 @app.route('/regist',methods=['POST'])
 def regist():
@@ -33,9 +32,13 @@ def regist():
     if result['flag'] == 1:
         return jsonify({'message':'两次输入密码不一致'})
     if result['flag'] == 'success':
-        return jsonify({'message':'注册成功'})
+        return jsonify({'message':'注册成功,请登陆'})
 
 @app.route('/regist_login')
 def regist_login():
     return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
